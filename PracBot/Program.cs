63a24d1b50550553bce7c93abdb51752ee93d87f
Sean Coleman <seancoleman13@gmail.com>
2020-08-22 02:31:39 -0600

    Add project files.

---
+++ PracBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace PracBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            var token = "NzQ2MTkwMzc5NTE0MzMxMTU2.Xz8t8g.Nb5CeUdiS_ETcw5JqT-r4R-9ZL4";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+
+
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
